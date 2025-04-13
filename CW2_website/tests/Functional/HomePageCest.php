f01c20cc6e01a1c00183bddbbbae9e45397468b8
Greta <25393081@edgehill.ac.uk>
2025-04-13 13:13:25 +0000

    Created some functional and acceptance test files, not all full. Also created the env.testing file

---
+++ CW2_website/tests/Functional/HomePageCest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Functional;
+use Tests\Support\FunctionalTester;
+
+final class HomePageCest
+{
+    public function routeExists(FunctionalTester $I)
+    {
+        $I->amOnPage('/index');
+        $I->seeResponseCodeIs(200);
+    }
+
+    public function correctViewLoaded(FunctionalTester $I)
+    {
+        $I->amOnPage('/index');
+        $I->see('Home', h1);
+    }
+
+    public function containsExpectedKeywords(FunctionalTester $I)
+    {
+        $I->amOnPage('/index');
+        $I->see('Welcome');
+        $I->see('questionnaires');
+    }
+
+    public function pageHasValidHtml(FunctionalTester $I)
+    {
+        $I->amOnPage('/index');
+        $I->seeElement('html');
+        $I->seeElement('nav');
+        $I->seeElement('body');
+        $I->seeElement('footer');
+    }
+
+    public function csrfTokenExists(FunctionalTester $I)
+    {
+        $I->amOnPage('/index');
+        $I->seeInSource('csrf-token');
+    }
+
+    public function seeQuestionnairesSection(FunctionalTester $I)
+    {
+        $questionnaire = $I->haveInDatabase('questionnaire', [
+            'title' => 'Test Questionnaire',
+            'description' => 'Example Questionnaire Description',
+            'status' => 'In development',
+        ]);
+
+        $I->seeInDatabase('questionnaire', ['id' => $questionnaire]);
+
+        $I->amOnPage('/index');
+        $I->see('Available questionnaires', 'h2');
+        $I->see('Test Questionnaire', 'h3');
+        $I->see('Example Questionnaire Description', 'p');
+        $I->see('In development', 'p');
+    }
+
+    public function seeNoQuestionnairesMessage(FunctionalTester $I)
+    {
+        $I->amOnPage('/index');
+        $I->see('Available questionnaires', 'h2');
+        $I->see('No questionnaires available at this time', 'p');
+    }
+}
