1f4b7bdb1d58cdd90547ed711dad0dde42c1b3ef
Greta <25393081@edgehill.ac.uk>
2025-04-24 14:38:11 +0000

    Updated routes and the testing env files

--- CW2_website/routes/web.php
+++ CW2_website/routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/index', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::get('/signup', [LoginController::class, 'create'])->name('signup');
+
+Route::prefix('user')->group(function(){
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
